8e2c49c06af0fc4e9cfe8849a9b772d1d813cc39
Anay Mehra <anaymehra63@gmail.com>
2024-09-20 06:06:29 +0530

    cors issue

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ connectDB();
 app.use(express.json());
 
 const allowedOrigins = [
-    "http://localhost:5173", // Your local frontend
     "https://google-docs-clone-red.vercel.app" // Your deployed frontend
   ];
 
